10a1e6e8260504fc7926e2b197753f934a3879e2
FBEZ <francesco.bez@gmail.com>
2025-09-19 11:45:05 +0200

    changed to iterations

--- explaination_fpu/main/main.c
+++ explaination_fpu/main/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "hal/cpu_hal.h"
 
+// ============================================================
+// Configuration
+// ============================================================
+#define ITERATIONS 10000
+
 void app_main(void)
 {
     // ============================================================
@@ -12,24 +17,22 @@ void app_main(void)
     double sum_d = 0;
     double division_d = 0;
 
-    uint32_t start_sum_d = esp_cpu_get_cycle_count();
-    sum_d = a_d + b_d;
-    uint32_t end_sum_d = esp_cpu_get_cycle_count();
+    uint32_t start_total_sum_d = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        sum_d = a_d + b_d;
+    }
+    uint32_t end_total_sum_d = esp_cpu_get_cycle_count();
 
-    uint32_t start_division_d = esp_cpu_get_cycle_count();
-    division_d = a_d / b_d;
-    uint32_t end_division_d = esp_cpu_get_cycle_count();
+    uint32_t start_total_div_d = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        division_d = a_d / b_d;
+    }
+    uint32_t end_total_div_d = esp_cpu_get_cycle_count();
 
     printf("\n=== DOUBLE ===\n");
-    printf("Sum took %lu cycles\n", (end_sum_d - start_sum_d));
-    printf("Division took %lu cycles\n", (end_division_d - start_division_d));
-
-    printf("*** Values ***\n");
-    printf("a: %f\nb: %f\n", a_d, b_d);
-
-    printf("*** Results ***\n");
-    printf("Sum: %f\nDivision: %f\n", sum_d, division_d);
-
+    printf("Sum average: %lu cycles\n", (end_total_sum_d - start_total_sum_d) / ITERATIONS);
+    printf("Division average: %lu cycles\n", (end_total_div_d - start_total_div_d) / ITERATIONS);
+    printf("Sum: %f, Division: %f\n", sum_d, division_d);
 
     // ============================================================
     // Float
@@ -40,24 +43,22 @@ void app_main(void)
     float sum_f = 0;
     float division_f = 0;
 
-    uint32_t start_sum_f = esp_cpu_get_cycle_count();
-    sum_f = a_f + b_f;
-    uint32_t end_sum_f = esp_cpu_get_cycle_count();
+    uint32_t start_total_sum_f = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        sum_f = a_f + b_f;
+    }
+    uint32_t end_total_sum_f = esp_cpu_get_cycle_count();
 
-    uint32_t start_division_f = esp_cpu_get_cycle_count();
-    division_f = a_f / b_f;
-    uint32_t end_division_f = esp_cpu_get_cycle_count();
+    uint32_t start_total_div_f = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        division_f = a_f / b_f;
+    }
+    uint32_t end_total_div_f = esp_cpu_get_cycle_count();
 
     printf("\n=== FLOAT ===\n");
-    printf("Sum took %lu cycles\n", (end_sum_f - start_sum_f));
-    printf("Division took %lu cycles\n", (end_division_f - start_division_f));
-
-    printf("*** Values ***\n");
-    printf("a: %f\nb: %f\n", a_f, b_f);
-
-    printf("*** Results ***\n");
-    printf("Sum: %f\nDivision: %f\n", sum_f, division_f);
-
+    printf("Sum average: %lu cycles\n", (end_total_sum_f - start_total_sum_f) / ITERATIONS);
+    printf("Division average: %lu cycles\n", (end_total_div_f - start_total_div_f) / ITERATIONS);
+    printf("Sum: %f, Division: %f\n", sum_f, division_f);
 
     // ============================================================
     // Int
@@ -68,21 +69,20 @@ void app_main(void)
     int sum_i = 0;
     int division_i = 0;
 
-    uint32_t start_sum_i = esp_cpu_get_cycle_count();
-    sum_i = a_i + b_i;
-    uint32_t end_sum_i = esp_cpu_get_cycle_count();
+    uint32_t start_total_sum_i = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        sum_i = a_i + b_i;
+    }
+    uint32_t end_total_sum_i = esp_cpu_get_cycle_count();
 
-    uint32_t start_division_i = esp_cpu_get_cycle_count();
-    division_i = a_i / b_i;
-    uint32_t end_division_i = esp_cpu_get_cycle_count();
+    uint32_t start_total_div_i = esp_cpu_get_cycle_count();
+    for (int i = 0; i < ITERATIONS; i++) {
+        division_i = a_i / b_i;
+    }
+    uint32_t end_total_div_i = esp_cpu_get_cycle_count();
 
     printf("\n=== INT ===\n");
-    printf("Sum took %lu cycles\n", (end_sum_i - start_sum_i));
-    printf("Division took %lu cycles\n", (end_division_i - start_division_i));
-
-    printf("*** Values ***\n");
-    printf("a: %d\nb: %d\n", a_i, b_i);
-
-    printf("*** Results ***\n");
-    printf("Sum: %d\nDivision: %d\n", sum_i, division_i);
+    printf("Sum average: %lu cycles\n", (end_total_sum_i - start_total_sum_i) / ITERATIONS);
+    printf("Division average: %lu cycles\n", (end_total_div_i - start_total_div_i) / ITERATIONS);
+    printf("Sum: %d, Division: %d\n", sum_i, division_i);
 }
